12bd185ba24c0330aa0afc5f59b879ccb4d1673d
russellasagna <laganzon.r2@yahoo.com>
2022-09-02 19:43:41 -0400

    Cleanup

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,9 +10,7 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <Link to='/orders'>Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to='/orders/new'>New Order</Link>
+      <Link to='/'>Component</Link>
       &nbsp; | &nbsp;
       Welcome, {user.name}
       &nbsp; | &nbsp;
