e3305affbca52d016bbfb75a10a717ede548d431
russellasagna <laganzon.r2@yahoo.com>
2022-09-07 19:25:51 -0400

    Update FeaturedPage component, featuresCtrl.show

--- controllers/api/features.js
+++ controllers/api/features.js
@@ -1,7 +1,17 @@
+const SpotifyAPI = require('../');
+
 module.exports = {
     show,
 };
 
-function show(req, res) {
+const BASE_URL = 'https://api.spotify.com';
 
+function show(req, res) {
+    const options = {
+        headers: {
+          'Authorization': '',
+          'Content-Type': 'application/json',
+        }
+    };
+    const songs = await fetch(BASE_URL, options).then(res => res.json());
 }
--- src/pages/FeaturedPage/FeaturedPage.jsx
+++ src/pages/FeaturedPage/FeaturedPage.jsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from 'react';
+import * as spotifyAPI from '../../utilities/spotify-api';
 import './FeaturedPage.css';
 
 export default function FeaturedPage() {
-
-  return (
-    <></>
-  );
+    const [songs, setSongs] = useState([]);
+    useEffect(function() {
+        async function getSong() {
+            const songs = await spotifyAPI.getTracks();
+            setSongs(songs);
+        }
+        getSong();
+    }, []);
+    return (
+        <div>
+            <br />
+            Spotify JSON:
+            <br />
+        </div>
+    );
 }
