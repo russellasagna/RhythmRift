f0e4d24dd96aa4e1341e9aef9b5737e4d57281f0
russellasagna <laganzon.r2@yahoo.com>
2022-09-09 18:29:03 -0400

    Disable JSON.stringify

--- src/components/JSONString/JSONString.jsx
+++ src/components/JSONString/JSONString.jsx
@@ -14,7 +14,7 @@ export default function JSONString() {
     return (
         <>
             <p style={{ color: "red" }}>
-                {JSON.stringify(results[0])}
+                {/* {JSON.stringify(results[0])} */}
             </p>
         </>
     );
