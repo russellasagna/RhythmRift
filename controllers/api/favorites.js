df08dd1d6fefff51b27795771b50a6d7fe0db0ce
russellasagna <laganzon.r2@yahoo.com>
2022-09-14 11:38:27 -0400

    Cleanup

--- controllers/api/favorites.js
+++ controllers/api/favorites.js
@@ -14,30 +14,16 @@ async function getFavorites(req, res) {
     res.json(songs);
 }
 
-// Add an item to the favorites
 async function addFavorite(req, res) {
-    // console.log(req.body);
     const song = await Song.findOne({trackId: req.body.trackId});
     if (song) {
         song.users.push(req.user._id);
         await song.save();
         res.json(song);
     } else {
-        // req.body.users(req.user._id);
         const newSong = new Song(req.body.data);
         newSong.users = req.user._id;
         await newSong.save();
         res.json(newSong);
     }
-}
-
-async function deleteFavorite(req, res) {
-    // const song = await Song.findOne({trackId: req.body.trackId});
-    // console.log(req.body.data);
-    // if (song) {
-    //     song.users.pop(req.user._id);
-    //     await song.save();
-    //     res.json(song);
-    // }
-    // res.json(song);
 }
