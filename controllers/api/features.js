c65e877fed8aa44c92fe0da929e7ba6d3dc42b1c
russellasagna <laganzon.r2@yahoo.com>
2022-09-13 23:27:13 -0400

    Update featuresCtrl; Style FeaturedPage component

--- controllers/api/features.js
+++ controllers/api/features.js
@@ -7,12 +7,6 @@ module.exports = {
 async function getSongs(req, res) {
     console.log(req.body.query);
     const BASE_URL = `https://itunes.apple.com/search?media=music&term=${req.body.query}&limit=10`;
-    // const options = {
-    //     headers: {
-    //       'Authorization': '',
-    //       'Content-Type': 'application/json',
-    //     }
-    // };
     const songs = await fetch(BASE_URL).then(res => res.json());
     console.log(songs);
     res.json(songs.results);
