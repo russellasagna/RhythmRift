41157fb2ab886d624a7dfe3644c4d5cb73ee2e8f
russellasagna <laganzon.r2@yahoo.com>
2022-09-14 11:52:14 -0400

    Fix ExplorePage component

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -3,7 +3,7 @@ import * as musicAPI from '../../utilities/music-api';
 import * as favoritesAPI from '../../utilities/favorites-api';
 import './ExplorePage.css';
 
-export default function ExplorePage({user}) {
+export default function ExplorePage({ user }) {
     async function getSongs(e) {
         e.preventDefault();
         const allSongs = await musicAPI.getTracks(query);
@@ -27,12 +27,12 @@ export default function ExplorePage({user}) {
             <form onSubmit={getSongs}>
                 <button type="submit">Search</button>
                 <div>
-                <input
-                    type="text" name='keyword' value={query}
-                    onChange={(evt) => setQuery(evt.target.value)}
-                    style={{ "textAlign": "center" }}
-                    autoComplete="off" placeholder="Enter keywords..."
-                    spellCheck="false"
+                    <input
+                        type="text" name='keyword' value={query}
+                        onChange={(evt) => setQuery(evt.target.value)}
+                        style={{ "textAlign": "center" }}
+                        autoComplete="off" placeholder="Enter keywords..."
+                        spellCheck="false"
                     />
                 </div>
             </form>
@@ -48,7 +48,10 @@ export default function ExplorePage({user}) {
                             </div>
                             <img src={r.artworkUrl100}></img>
                             <a href={r.trackViewUrl} target={'_blank'}>See on Apple Music</a>
-                                {/* {user} ? <button onClick={() => handleAddFavorite(r)} type="submit">Add Favorite</button> */}
+                            {user ? <button onClick={() => handleAddFavorite(r)} type="submit">Add Favorite</button>
+                                :
+                                <div></div>
+                            }                   
                         </a>
                     </>
                 )}
